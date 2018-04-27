9e6dc22d600933f90957f07387b17cb429c160a3
Pranav Jayaraj <pranavjayaraj7@gmail.com>
2018-04-27 10:46:32 +0530

    Update DrawView.java

--- app/src/main/java/com/pranavj7/mnist/view/DrawView.java
+++ app/src/main/java/com/pranavj7/mnist/view/DrawView.java
@@ -5,7 +5,6 @@ import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.View;
 
-
 public class DrawView extends View {
     private Paint mPaint = new Paint();
     private DrawModel mModel;
